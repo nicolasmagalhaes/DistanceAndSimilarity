2148957d8a1bb0e5d0bb1a528b6cdee94840e4b7
Nicolas Magalhães <magalhaesnicolas23@gmail.com>
2020-07-02 17:12:34 -0300

    fix

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Test
 
 @testset "Function test" begin
 
-    @test EuclidianDistance([1,1],[3,3]) == 4
+    @test euclidianDistance([1,1],[3,3]) == 4
     @test manhattanDistance([3,45,7,2],[2,54,13,15]) == 29
     @test cosineDistance([3,45,7,2],[2,54,13,15]) == 0.02771574828765011
     @test minkowskiDistance([1,2,3],[1,2,3],2) == 0
